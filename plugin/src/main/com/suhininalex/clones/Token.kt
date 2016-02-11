931fb5f0adc6e5c6ec19375927858eeada22cde1
suhininalex <suhininalex@gmail.com>
2016-02-11 13:49:06 +0300

    nonAnonimized methods

--- plugin/src/main/com/suhininalex/clones/Token.kt
+++ plugin/src/main/com/suhininalex/clones/Token.kt
@@ -2,10 +2,18 @@ package com.suhininalex.clones
 
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiMethod
+import com.intellij.psi.impl.source.tree.ElementType
+import com.intellij.psi.tree.TokenSet
+import net.suhininalex.kotlin.utils.abbrevate
 
 class Token(val source: PsiElement, val method: PsiMethod) : Comparable<Token> {
 
-    val value: Short = source.node.elementType.index
+    val nonAnonimized = source.notNeedAnonimization()
+
+    val value: Int =
+        if (nonAnonimized)
+            source.text.hashCode()
+        else source.node.elementType.index.toInt()
 
     override fun hashCode() = value.hashCode()
 
@@ -15,5 +23,12 @@ class Token(val source: PsiElement, val method: PsiMethod) : Comparable<Token> {
         if (other is Token) value == other.value
         else false
 
-    override fun toString() = value.toString()
+    override fun toString() =
+        if (nonAnonimized) source.text.abbrevate(5)
+        else source.node.elementType.toString()
 }
+
+fun PsiElement.notNeedAnonimization() =
+    parent.parent in TokenSet.create(ElementType.METHOD_CALL_EXPRESSION) &&
+    parent in TokenSet.create(ElementType.REFERENCE_EXPRESSION) &&
+    this in TokenSet.create(ElementType.IDENTIFIER)
