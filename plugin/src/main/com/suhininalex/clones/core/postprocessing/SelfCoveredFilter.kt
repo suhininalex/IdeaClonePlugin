e9ae2a3e3e7c943b9ceb384df0bae40bd79908f8
suhininalex <suhininalex@gmail.com>
2017-03-06 14:49:21 +0200

    improvement of selfCoverageFilter

--- plugin/src/main/com/suhininalex/clones/core/postprocessing/SelfCoveredFilter.kt
+++ plugin/src/main/com/suhininalex/clones/core/postprocessing/SelfCoveredFilter.kt
@@ -4,8 +4,9 @@ import com.suhininalex.clones.core.*
 import com.suhininalex.clones.core.structures.Clone
 import com.suhininalex.clones.core.structures.CloneClass
 import com.suhininalex.clones.core.structures.Token
-import com.suhininalex.clones.core.structures.TreeCloneClass
 import com.suhininalex.clones.core.utils.*
+import com.suhininalex.clones.ide.configuration.PluginSettings
+import com.suhininalex.suffixtree.SuffixTree
 import nl.komponents.kovenant.Promise
 import java.lang.Exception
 
@@ -30,18 +31,26 @@ private fun CloneClass.scoreSelfCoverage(): Int =
         clones.first().scoreSelfCoverage()
 
 private fun Clone.scoreSelfCoverage(): Int {
-    val sequence = tokenSequence().toList()
-    val indexMap = sequence.mapIndexed { i, psiElement ->  psiElement to i}.toMap()
-    val length = com.suhininalex.clones.core.utils.suffixTree(sequence.map(::Token).toList())
-            .getAllCloneClasses(10).toList()
-            .filterSubClassClones()
-            .flatMap { it.clones.toList() }
-            .map{ IntRange(indexMap[it.firstPsi]!!, indexMap[it.lastPsi]!!) }
-            .uniteRanges()
-            .sumBy { it.length }
-    val bigLength = sequence.size
-    return length*100/bigLength
+
+     val sequence = tokenSequence().toList()
+     val indexMap = sequence.mapIndexed { i, psiElement ->  psiElement to i}.toMap()
+     val tree = suffixTree(sequence.map(::Token).toList())
+
+     if (tree.haveTooMuchClones(sequence.size)) return 100
+
+     val length = tree
+             .getAllCloneClasses(10).toList()
+             .filterSubClassClones()
+             .flatMap { it.clones.toList() }
+             .map{ IntRange(indexMap[it.firstPsi]!!, indexMap[it.lastPsi]!!) }
+             .uniteRanges()
+             .sumBy { it.length }
+     return length*100/sequence.size
 }
+
+private fun SuffixTree<Token>.haveTooMuchClones(sourceLength: Int) =
+    getAllCloneClasses(10).drop(sourceLength*PluginSettings.coverageSkipFilter/100).firstOrNull() != null
+
 
 private fun CloneClass.scoreSameMethod(): Int {
     assert(size > 1)
