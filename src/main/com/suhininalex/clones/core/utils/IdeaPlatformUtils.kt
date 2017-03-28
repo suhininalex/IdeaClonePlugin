dbca7535520a1d1fb1766eeedc63f1b997e2e41f
suhininalex <suhininalex@gmail.com>
2017-03-28 13:39:57 +0300

    Cleanup: removing of unused functions

--- src/main/com/suhininalex/clones/core/utils/IdeaPlatformUtils.kt
+++ src/main/com/suhininalex/clones/core/utils/IdeaPlatformUtils.kt
@@ -8,7 +8,6 @@ import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.util.Computable
-import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowManager
 import com.intellij.psi.*
 import com.intellij.psi.impl.source.tree.ElementType
@@ -155,12 +154,6 @@ val CurrentProject: Project?
             if (isDisposed) null
             else this
         }
-
-fun ToolWindow.hide(){
-    hide(null)
-}
-
-fun ToolWindow.show(){}
 
 val Project.toolWindowManager: ToolWindowManager
     get() = ToolWindowManager.getInstance(this)
