46a3c77bfc64c0cfb1375f3609108f8a7d79bd3c
suhininalex <suhininalex@gmail.com>
2017-04-03 16:40:01 +0300

    fix: cropping unused kotlin braces

--- src/main/com/suhininalex/clones/core/postprocessing/helpers/NoiseCropper.kt
+++ src/main/com/suhininalex/clones/core/postprocessing/helpers/NoiseCropper.kt
@@ -1,30 +1,39 @@
 package com.suhininalex.clones.core.postprocessing.helpers
 
+import com.intellij.psi.PsiElement
 import com.intellij.psi.impl.source.tree.ElementType
 import com.intellij.psi.tree.TokenSet
 import com.suhininalex.clones.core.structures.Clone
 import com.suhininalex.clones.core.structures.PsiRange
 import com.suhininalex.clones.core.structures.RangeClone
 import com.suhininalex.clones.core.utils.contains
+import org.jetbrains.kotlin.lexer.KtTokens
 
 fun PsiRange.cropBadTokens(): Clone {
     var left = firstPsi
     var right = lastPsi
-    while ( ! (left in lBraces && left.parent.lastChild.textRange.endOffset <= lastPsi.textRange.endOffset) && left in badTokens && left.nextSibling != null) {
-        left = left.nextSibling
+    while (isUnclosedLeftBrace(left) || left in badTokens) {
+        left = left.nextSibling ?: break
     }
-    while (! (right in rBraces && right.parent.firstChild.textRange.startOffset >= firstPsi.textRange.startOffset) && right in badTokens && right.prevSibling != null) {
-        right = right.prevSibling
+    while (isUnclosedRightBrace(right) || right in badTokens) {
+        right = right.prevSibling ?: break
     }
     return RangeClone(left, right)
 }
 
+private fun PsiRange.isUnclosedLeftBrace(psiElement: PsiElement): Boolean =
+        psiElement in lBraces && psiElement.parent.lastChild.textRange.endOffset > lastPsi.textRange.endOffset
+
+private fun PsiRange.isUnclosedRightBrace(psiElement: PsiElement): Boolean =
+        psiElement in rBraces && psiElement.parent.firstChild.textRange.startOffset < firstPsi.textRange.startOffset
+
 private val badTokens: TokenSet = TokenSet.create(
-        ElementType.WHITE_SPACE, ElementType.DOC_COMMENT, ElementType.C_STYLE_COMMENT, ElementType.END_OF_LINE_COMMENT, ElementType.SEMICOLON, ElementType.RPARENTH, ElementType.LPARENTH, ElementType.RBRACE, ElementType.LBRACE, ElementType.EXPRESSION_LIST, ElementType.COMMA
+        ElementType.WHITE_SPACE, ElementType.DOC_COMMENT, ElementType.C_STYLE_COMMENT, ElementType.END_OF_LINE_COMMENT, ElementType.SEMICOLON, ElementType.EXPRESSION_LIST, ElementType.COMMA
 )
 
+//TODO generalize this part
 private val lBraces: TokenSet =
-        TokenSet.create(ElementType.LPARENTH, ElementType.LBRACE, ElementType.LBRACKET)
+        TokenSet.create(ElementType.LPARENTH, ElementType.LBRACE, ElementType.LBRACKET, KtTokens.LBRACE, KtTokens.LPAR, KtTokens.RBRACKET)
 
 private val rBraces: TokenSet =
-        TokenSet.create(ElementType.RPARENTH, ElementType.RBRACE, ElementType.RBRACKET)
+        TokenSet.create(ElementType.RPARENTH, ElementType.RBRACE, ElementType.RBRACKET, KtTokens.RBRACE, KtTokens.RPAR, KtTokens.RBRACKET)
