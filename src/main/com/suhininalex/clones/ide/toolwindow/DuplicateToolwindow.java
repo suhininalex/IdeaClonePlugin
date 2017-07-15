c197ec3b30a73c4a5fd88b28c745536863455c41
suhininalex <suhininalex@gmail.com>
2017-07-15 17:11:30 +0300

    difftoolwindow main panel

---
+++ src/main/com/suhininalex/clones/ide/toolwindow/DuplicateToolwindow.java
@@ -0,0 +1,11 @@
+package com.suhininalex.clones.ide.toolwindow;
+
+import javax.swing.*;
+
+
+public class DuplicateToolwindow {
+    public JPanel panel;
+    public JPanel reportPanel;
+    public JPanel diffPanel;
+
+}
